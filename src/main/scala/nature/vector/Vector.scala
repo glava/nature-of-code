2fdc91b41b4a73216f2e59017f7ff22a6c300f21
Goran Ojkic <goran.ojkic@here.com>
2015-02-22 21:41:11 +0100

    Finished mover class 1.7

--- src/main/scala/nature/vector/Vector.scala
+++ src/main/scala/nature/vector/Vector.scala
@@ -1,8 +1,43 @@
 import processing.core._
 
-object BouncingBall extends App {
+case class PVector(x: Float, y: Float) {
 
-   PRunner.run(new BouncingBall, "Bouncing Ball")
+  def +(v: PVector) = PVector(x + v.x, y + v.y)
+
+  def -(v: PVector) = PVector(x - v.x, y - v.y)
+
+  def *(n: Float) = PVector(x * n, y * n)
+
+  def /(n: Float) = PVector(x / n, y / n)
+
+  def mag = Math.sqrt(x * x + y * y).toFloat
+
+  def normalize  = if (mag > 0) this / mag else this
+
+  override def toString = s"vx = $x ; vy = $y"
+}
+
+object ZeroVector extends PVector(0, 0)
+
+
+class VectorSubtraction extends PApplet {
+
+  override def draw() = {
+    background(255)
+
+    val lineVector = (PVector(mouseX, mouseY) - PVector(width / 2, height / 2)) * 0.5f
+
+    fill(0)
+    rect(0f, 0f, PVector(mouseX, mouseY).mag, 10f )
+
+    translate(width / 2, height / 2)
+    line(0, 0, lineVector.x, lineVector.y)
+  }
+
+}
+
+object VectorSubtraction extends App {
+  PRunner.run(new VectorSubtraction, "Subtraction")
 }
 
 class BouncingBall extends PApplet {
@@ -22,12 +57,12 @@ class BouncingBall extends PApplet {
 
     println(ballLocation)
 
-    if((ballLocation.x > width) || (ballLocation.x < 0)) {
+    if ((ballLocation.x > width) || (ballLocation.x < 0)) {
       velocity = velocity.copy(x = (-1) * velocity.x)
     }
 
-    if((ballLocation.y > height) || (ballLocation.y < 0)) {
-      velocity  = velocity.copy(y = (-1) * velocity.y)
+    if ((ballLocation.y > height) || (ballLocation.y < 0)) {
+      velocity = velocity.copy(y = (-1) * velocity.y)
     }
 
     stroke(0)
@@ -37,9 +72,60 @@ class BouncingBall extends PApplet {
   }
 }
 
-case class PVector(x: Float, y: Float) {
-  def +(v: PVector) = PVector(x + v.x, y + v.y)
+case class Mover(location: PVector, velocity: PVector, applet: PApplet) {
 
-  override def toString = s"vx = $x ; vy = $y"
+  def move = Mover(location + velocity, velocity, applet)
+
+  def display() = {
+    applet.stroke(0)
+    applet.fill(175)
+    applet.ellipse(location.x, location.y, 16, 16)
+  }
+
+  def keepInBounds =  {
+    val r =
+    if (location.x > applet.width) {
+      copy(location = location.copy(x = 0))
+    } else if (location.x < 0) {
+      copy(location = location.copy(x = applet.width))
+    } else this
+
+    if (r.location.y > applet.height) {
+      copy(location = location.copy(y = 0))
+    } else if (location.y < 0) {
+      copy(location = location.copy(y = applet.height))
+    } else r
+  }
 }
+
+class MoverBouncer extends PApplet {
+
+  var mover = Mover(
+    PVector(random(width),random(height)) ,
+    PVector(random(-2,2),random(-2,2)),
+    this
+  )
+
+  override def setup() = {
+    size(640, 360)
+    background(255)
+  }
+
+  override def draw() = {
+    background(255)
+    mover = mover.move.keepInBounds
+    mover.display()
+  }
+}
+
+object MoverBouncer extends App {
+  PRunner.run(new MoverBouncer, "Move bouncer")
+}
+
+object BouncingBall extends App {
+  PRunner.run(new BouncingBall, "Bouncing Ball")
+}
+
+
+
 
